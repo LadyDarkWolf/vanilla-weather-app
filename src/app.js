e8e5fdce12ccb25a4b3ebc10a417a6a05d978a7b
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-08-15 17:36:21 +1000

    Start updating temperature, windspeed etc from real data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+let apiKey = "2af1ff2de81cdd8d67552da7d4b4331d";
 
+let baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=Sydney,AU&units=metric`;
+
+function displayWeather(weather) {
+  let currentTempField = document.querySelector("#current-temperature");
+  currentTempField.innerHTML = Math.round(weather.data.main.temp);
+  let locationField = document.querySelector("#location");
+  locationField.innerHTML = `${weather.data.name},&nbsp;${weather.data.sys.country}`;
+  let conditionsField = document.querySelector("#conditions");
+  conditionsField.innerHTML = weather.data.weather[0].description;
+  let humidityField = document.querySelector("#humidity");
+  humidityField.innerHTML = weather.data.main.humidity;
+  let windspeedField = document.querySelector("#windspeed");
+  windspeedField.innerHTML = Math.round(weather.data.wind.speed);
+}
+axios.get(baseUrl).then(displayWeather);
